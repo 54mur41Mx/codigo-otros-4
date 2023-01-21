6c2094c696cf7aa4225ffade4952e77fd1657017
54mur41mx <c.albert15va@gmail.com>
2023-01-21 03:18:33 -0600

    Piedra papel o tijeras

--- Ejerciciojava4.java
+++ Ejerciciojava4.java
@@ -1,39 +1,52 @@
 package com.generation;
 
+import java.util.Scanner;
+
 public class Codigo4 {
 
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+	    
+	    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+	    String j1;
+	    j1 = s.nextLine();
+	    
+	    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+	    String j2;
+	    j2 = s.nextLine();
+	    
+	    if (j1.equals(j2)) {
+	      System.out.println("Empate");
+	    } 
+	    
+	    else {
+	      int g = 2;
+	      
+	      switch(j1) {
+	        case "piedra":
+	          if (j2.equals("tijeras")) {
+	            g = 1;
+	          }
+	          break;
 
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
-    }
-  
-  
+	        case "papel":
+	          if (j2.equals("piedra")) {
+	            g = 1;
+	          }
+	          break;
+	        case "tijera":
+	          if (j2.equals("papel")) {
+	            g = 1;
+	          }
+	          break;
+	        default:
+	      }
+	      System.out.println("Gana el jugador " + g);
+	      s.close();
+	      
+	    }
+	    
+	  
+	}
+
 }
